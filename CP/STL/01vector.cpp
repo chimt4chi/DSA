2f300fc2cfe667a4bb4cf0aaf25d87c33df5cb34
Rohit Singh <rs9990271.edu@gmail.com>
2024-09-25 19:47:07 +0530

    array stl

--- CP/STL/01vector.cpp
+++ CP/STL/01vector.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+
+  // Q: What is a vector?
+  // A: Vector is a sequence container that is an array that can change its size dynamically. Unlike array the size of vector can be modified at the runtime.
+
+  // vector<int> v;        // Declation of vector v.
   vector<int> v(5, 20); // vector v of int size of and 20 for each index
   v.push_back(1);       // adds the element in the end
   v.emplace_back(2);    // efficient than push_back()
@@ -17,9 +22,11 @@ int main()
   //     cout << v[i];
   // }
 
-  // for(vector<int>::iterator i = v.begin(); i != v.end(); i++){
+  // for(vector<int>::iterator i = v.begin(); i != v.end(); i++){ // iterating through all the elements in the vector
   //     cout << *i;
   // }
+
+  // iterators
 
   vector<int>::iterator i = v.begin();
   i++;
